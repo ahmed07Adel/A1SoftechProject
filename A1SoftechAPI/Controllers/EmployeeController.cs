159a0f6b86a74cd99ab4a31ee47abed896431d86
Ahmed Adel Mohamed <63484238+ahmed07Adel@users.noreply.github.com>
2022-06-30 17:45:01 +0200

    Add project files.

---
+++ A1SoftechAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Core.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace A1SoftechAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository repository;
+
+        public EmployeeController(IEmployeeRepository repository)
+        {
+            this.repository=repository;
+        }
+        [HttpPost("CreateEmployee")]
+        public async Task<ActionResult<Employee>> CreateEmployee([FromForm] EmployeeViewModel model)
+        {
+            try
+            {
+                if (model == null)
+                {
+                    return BadRequest();
+                }
+                var entity = new Employee
+                {
+                    Name = model.Name,
+                    Salary = model.Salary,
+                    Email = model.Email,
+                    Mobile = model.Mobile,
+                };
+                var res = await repository.CreateEmployee(entity);
+                return res;
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error Create data");
+            }
+        }
+    }
+}
---
+++ Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,72 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Core.ViewModel;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly EmployeeDBContext context;
+        public EmployeeRepository(EmployeeDBContext context)
+        {
+            this.context=context;
+        }
+
+        public Task<EmployeeTax> CalculateEmployeeTax(EmployeeTax CalcEmployeeTax)
+        {
+            
+        }
+
+        public async Task<Employee> CreateEmployee(Employee newemployee)
+        {
+            var res = await context.Employees.AddAsync(newemployee);
+            await context.SaveChangesAsync();
+            return res.Entity;
+        }
+
+
+        public async Task<Employee> DeleteEmployee(int EmployeeID)
+        {
+            var res = await context.Employees.FirstOrDefaultAsync(e => e.Id == EmployeeID);
+            if (res != null)
+            {
+                context.Employees.Remove(res);
+                await context.SaveChangesAsync();
+                return res;
+            }
+            return null;
+        }
+
+        public async Task<IEnumerable> GetAllEmployees()
+        {
+            var res = await context.Employees.ToListAsync();
+            return res;
+        }
+
+        public async Task<Employee> GetEmployeeByID(int EmployeeID)
+        {
+            var res = await context.Employees.FirstOrDefaultAsync(x => x.Id == EmployeeID);
+            return res;
+        }
+
+        public async Task<Employee> UpdateEmployee(EmployeeViewModel employee)
+        {
+            var res = await context.Employees.FirstOrDefaultAsync(x => x.Id == employee.Id);
+            if (res != null)
+            {
+                res.Name = employee.Name;
+                await context.SaveChangesAsync();
+                return res;
+            }
+            return null;
+        }
+    }
+}
